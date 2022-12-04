6801cdbe0b6544170233cebd0ab06c3420656cca
lifthoofd <eovermeire@gmail.com>
2022-12-04 10:25:59 +0100

    added start scripts

---
+++ scripts/four_screens.sh
@@ -0,0 +1,2 @@
+/usr/bin/gnome-terminal --geometry=1920x1080+5760+0 --full-screen --hide-menubar -- /usr/bin/python3 /home/USER/PATH_TO_REPO/HTMAO_BOT/htmao_headlinalyser/main.py
+/home/USER/PATH_TO_OPENFRAMEWORKS/apps/myApps/ofHTMAO/ofHTMAO_player/bin/ofHTMAO_player
---
+++ scripts/three_screens.sh
@@ -0,0 +1,2 @@
+/usr/bin/python3 /home/USER/PATH_TO_REPO/HTMAO_BOT/htmao_headlinalyser/main.py &
+/home/USER/PATH_TO_OPENFRAMEWORKS/apps/myApps/ofHTMAO/ofHTMAO_player/bin/ofHTMAO_player
